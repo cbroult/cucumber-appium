003480f2bacb174f2e9b67a78266b250cba98a31
Christophe Broult <christophe@cbroult-mbp.local>
2015-01-22 14:49:10 -0500

    Updated the capabilities for iOS 8.1 device and 1.3.4 Appium.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,33 +1,49 @@
+require 'net/http'
 require 'rspec/expectations'
 require 'selenium-webdriver'
 
+ENABLED_NET_HTTP_TIMEOUT = false
+
+class Net::HTTP
+  alias_method  :__original_initialize__, :initialize
+
+  def initialize(address, port = nil)
+    __original_initialize__(address, port)
+    @read_timeout = 300
+  end
+end if ENABLED_NET_HTTP_TIMEOUT
+
 APP_PATH = '/../../PlainNote/build/Release-iphonesimulator/PlainNote.app'
-USERNAME = 'ShashikantJ'
-API_KEY = ''
+USERNAME = ENV['SAUCE_USERNAME']
+API_KEY = ENV['SAUCE_ACCESS_KEY']
+
 def capabilities
   {
-    'browserName' => 'iOS',
-    'platform' => 'Mac',
-    'version' => '6.0',
+    'deviceName' => 'iPhone 6',
+    'platformName' => 'iOS',
+    'platformVersion' => '8.1',
     'app' => absolute_app_path
   }
 end
 
 def sauce_capabilities
   {
-
+    'appiumVersion' => '1.3.4',
     'app' => 'sauce-storage:PlainNote.zip',
-    'device' => 'iPhone Simulator',
+    'deviceName' => 'iPhone Simulator',
     'username' => USERNAME,
     'access-key' => API_KEY,
     #'os' => 'iOS'
-    #'browserName' => 'iPhone',
-    'platform' => 'OS X 10.8',
-    'version' => '6.0',
+    'browserName' => '',
+    'platformName' => 'iOS',#'OS X 10.8',
+    'platformVersion' => '8.1',
     'name' => 'Running PlainNote wit Cucumber and Appium',
-    'passed' => 'true'
-
+#    'passed' => 'true',
+    'idle-timeout' => 300,
+    'max-duration' => 600,
+    'command-timeout' => 300,
   }
+   # 'idle_timeout] = 60  }
 end
 
 def absolute_app_path
